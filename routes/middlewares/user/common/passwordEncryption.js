470ab660ecf9ea41949a3b0e216c251429557ebf
k1oo <kloo21202@gmail.com>
2020-08-20 08:43:37 +0900

    encryption requested user password

---
+++ routes/middlewares/user/common/passwordEncryption.js
@@ -0,0 +1,25 @@
+import SHA512 from 'crypto-js/sha512';
+import Base64 from 'crypto-js/enc-base64';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const passwordEncryption = async (req, res, next) => {
+  const { password } = req.body;
+  const secretKey = process.env.PASSWORD_ENCRYPTION_KEY || '';
+
+  try {
+    const encryptedPassword = Base64.stringify(SHA512(password, secretKey));
+
+    res.locals.password = encryptedPassword;
+    next();
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'unhandled error',
+    });
+  }
+};
+
+export default passwordEncryption;
