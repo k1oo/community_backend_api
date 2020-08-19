1a0c526fb9b51efa7e71e8208453b7b0ffe99ad8
k1oo <kloo21202@gmail.com>
2020-08-20 08:40:22 +0900

    create query function

--- db.js
+++ db.js
@@ -1,4 +1,7 @@
 import { MySQL } from 'fxsql';
+import { curry, reduce, go } from 'fxjs';
+import { not, isFunction, match, identity, flatten, each } from 'fxjs/Strict';
+import L from 'fxjs/Lazy';
 import dotenv from 'dotenv';
 
 const { CONNECT } = MySQL;
@@ -11,3 +14,27 @@ export const POOL = CONNECT({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+export const { QUERY, EQ, SQL } = POOL;
+
+export const toEQ = (v) => {
+  return match(v).case(isFunction)(identity).else(EQ);
+};
+
+export const merge = curry((sep = SQL``, ...sqls) =>
+  go(
+    sqls,
+    flatten,
+    L.reject(not),
+    L.map(toEQ),
+    L.filter((sql) => sql().text),
+    ([head, ...tail]) =>
+      !head ? SQL`` : SQL`(${reduce((acc, sql) => SQL`${acc} ${sep} ${sql}`, head, tail)})`
+  )
+);
+
+export const or = merge(SQL`OR`);
+
+export const WQ = (sql) => SQL`WHERE ${toEQ(sql)}`;
+
+export const Wor = (...sqls) => WQ(or(...sqls));
