2665f19afdb8c8289cf505ec5bc403fb7c9f6c4c
k1oo <kloo21202@gmail.com>
2020-08-20 09:15:43 +0900

    issue token API

---
+++ routes/middlewares/jwt/issueToken.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const issueToken = async (req, res, next) => {
+  const { pk } = res.locals.user;
+
+  try {
+    const token = jwt.sign({ data: pk }, process.env.TOKEN_SECRET_KEY, { expiresIn: '1h' });
+
+    return res.json({
+      success: true,
+      token,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      code: 500,
+      message: 'unhandled error',
+    });
+  }
+};
+
+export default issueToken;
