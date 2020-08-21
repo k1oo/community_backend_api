25c50784c38eb8295956c53042fb70c2121bd04e
k1oo <kloo21202@gmail.com>
2020-08-21 22:00:44 +0900

    add select query functions

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
+import dotenv from 'dotenv';
 import { MySQL } from 'fxsql';
 import { curry, reduce, go } from 'fxjs';
 import { not, isFunction, match, identity, flatten, each } from 'fxjs/Strict';
 import L from 'fxjs/Lazy';
-import dotenv from 'dotenv';
 
 const { CONNECT } = MySQL;
 
@@ -15,7 +15,7 @@ export const POOL = CONNECT({
   database: process.env.DB_NAME,
 });
 
-export const { QUERY, EQ, SQL, SET } = POOL;
+export const { QUERY, EQ, SQL, SET, VALUES, CL, ASSOCIATE } = POOL;
 
 export const toEQ = (v) => {
   return match(v).case(isFunction)(identity).else(EQ);
@@ -34,11 +34,18 @@ export const merge = curry((sep = SQL``, ...sqls) =>
 );
 
 export const or = merge(SQL`OR`);
+export const and = merge(SQL`AND`);
+export const anD = (...sqls) => and(...sqls, no_del);
 
 export const WQ = (sql) => SQL`WHERE ${toEQ(sql)}`;
 
 export const Wor = (...sqls) => WQ(or(...sqls));
+export const WanD = (...sqls) => WQ(anD(...sqls));
 
 export const findUserByPk = (user_pk) => {
   return QUERY`SELECT * FROM users ${WQ({ pk: user_pk })}`;
 };
+
+const is_null = (cl) => SQL`${CL(cl)} IS NULL`;
+
+export const no_del = is_null('deleted_at');
